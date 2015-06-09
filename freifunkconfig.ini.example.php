676eacd0b5f6e13fbe81ffe92f8896265011d2c4
Guenther Schmitz <gpunktschmitz@aol.com>
2015-06-09 09:56:32 +0200

    initial script

---
+++ freifunkconfig.ini.example.php
@@ -0,0 +1,9 @@
+<?php exit; ?>
+[freifunkcrawler]
+title=freifunkstats
+dataDir=./freifunkdata
+totalDir=Total
+netmonUrl=https://netmon.freifunk-franken.de/api/rest/router/
+urlRequester=curl
+chownUser=
+chownGroup=
